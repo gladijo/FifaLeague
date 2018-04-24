51abc0378dd6c2665144f4eca7d64ceb5bca5f77
gladijo <jorden_gladines@hotmail.com>
2018-04-24 16:33:22 +0200

    selectable player

--- FifaLeague.UI/src/app/playerlist/playerlist.component.ts
+++ FifaLeague.UI/src/app/playerlist/playerlist.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Player } from '../models/player';
+import { FifaLeagueService } from '../fifa-league.service';
+import { PlayerformComponent } from '../playerform/playerform.component';
+import { MatDialog, MatDialogRef, MatDialogConfig } from '@angular/material';
 
 @Component({
   selector: 'app-playerlist',
@@ -8,11 +11,37 @@ import { Player } from '../models/player';
 })
 
 export class PlayerlistComponent implements OnInit {
-  @Input() public Players:Player[];
+  @Input() public _players:Player[];
   
-  constructor() { }
+  selectedPlayer:Player;
+
+  constructor(private _dialog:MatDialog, private _fifaleagueService:FifaLeagueService) { }
 
   ngOnInit() {    
-    
+    this.getPlayers();
   }  
+
+  getPlayers():void {
+    this._fifaleagueService.getPlayers().subscribe(data => this._players = data);    
+  }
+
+  select(player: Player): void {
+    this.selectedPlayer = player;
+    console.log(this.selectedPlayer);
+    this.openDialog();
+  }
+
+  openDialog():void {
+    const dialogConfig = new MatDialogConfig();
+      
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.data = { };
+  
+    let dialogRef = this._dialog.open(PlayerformComponent, dialogConfig);
+    
+    dialogRef.afterClosed().subscribe(result => { 
+       console.log('The dialog was closed.');       
+    });
+  }
 }
--- FifaLeague.UI/src/app/playerpage/playerpage.component.ts
+++ FifaLeague.UI/src/app/playerpage/playerpage.component.ts
@@ -13,16 +13,10 @@ import { Player } from '../models/player';
 })
 export class PlayerpageComponent implements OnInit {
 
-  private _players:Player[];
-
-  constructor(public dialog: MatDialog, private _fifaleagueService: FifaLeagueService) {}
+  constructor(public _dialog: MatDialog) {}
 
   ngOnInit() {
-    this.getPlayers();
-  }
-
-  getPlayers():void {
-    this._fifaleagueService.getPlayers().subscribe(data => this._players = data);    
+    
   }
 
   openDialog():void {
@@ -30,23 +24,12 @@ export class PlayerpageComponent implements OnInit {
       
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
-    dialogConfig.data = {
-        id: 1,
-        title: 'Angular For Beginners'
-    };
+    dialogConfig.data = { };
   
-    let dialogRef = this.dialog.open(PlayerformComponent, dialogConfig);
+    let dialogRef = this._dialog.open(PlayerformComponent, dialogConfig);
     
     dialogRef.afterClosed().subscribe(result => { 
-       console.log('The dialog was closed.');
-       // refresh data if needed
-       this.getPlayers();       
-    });
-  }
-
-  updatePlayer(model:Player):void {
-    this._fifaleagueService.updatePlayer(model).subscribe(data => { 
-
+       console.log('The dialog was closed.');       
     });
   }
 
