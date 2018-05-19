096c6f5ed4040e01be852abba91211332fc21a82
gladijo <jorden_gladines@hotmail.com>
2018-05-19 22:39:39 +0200

    useInMemory DB for unittests

--- FifaLeague.API.Tests/UnitTest1.cs
+++ FifaLeague.API.Tests/UnitTest1.cs
@@ -2,18 +2,25 @@ using System;
 using Xunit;
 using FifaLeague.API.Controllers;
 using Moq;
+using Microsoft.EntityFrameworkCore;
+using FifaLeague.API.Models;
 
 namespace FifaLeague.API.Tests
 {
     public class PlayerControllerTests
     {
-        private readonly PlayerController _controller;
+        DbContextOptions<FifaLeagueContext> options;
 
+        public PlayerControllerTests() {
+            var builder = new DbContextOptionsBuilder<FifaLeagueContext>();
+            builder.UseInMemoryDatabase("FifaLeagueDB");
+            options = builder.Options;
+        }
         [Fact]
         public void Test1()
         {
             // Arrange
-            var mockContext = new Mock<FifaLeague.API.Models.FifaLeagueContext>();
+            var mockContext = new Mock<FifaLeague.API.Models.FifaLeagueContext>(options);
             var controller = new PlayerController(mockContext.Object);
         
             // Act
